c40c1b0774bcecc1f3f9854e929125300b95f006
bisco <nobu+github@glleu.net>
2015-10-29 23:31:20 +0900

    rename file

--- public/js/perfmon_index.js
+++ public/js/perfmon_index.js
@@ -1,185 +1,131 @@
 $(function(){
-    (function(){
-        // smoothie.js + mpstat
-        var smoothie = new SmoothieChart(
-                           {timestampFormatter:SmoothieChart.timeFormatter,
-                            maxValue: 100, minValue: 0, 
-                            labels: {fontSize: 14},
-                            grid: { sharpLines:true,strokeStyle: 'rgb(0, 125, 0)',
-                            lineWidth: 1, //fillStyle: 'rgb(60, 0, 0)', 
-                            millisPerLine: 1000, verticalSections: 5 },
-                        });
-        var i;
-        var param_count = 4;
-        var line = [];
-        for(i=0;i<param_count;i++) {
-            line.push(new TimeSeries());
+  (function(){
+    // We use an inline data source in the example, usually data would
+    // be fetched from a server
+    var usr = [], sys = [], idle = [];
+    var d_rBs = [], d_wBs = [];
+    var n_rBs = [], n_wBs = [];
+    var plots = [];
+    var placeholders = ["#mpstat_total", "#diskstat_total", "#netstat_total"];
+    var leg_placeholders = ["#mpstat_legend", "#diskstat_legend", "#netstat_legend"];
+    var _TOTAL_POINTS = 50;
+
+    function get_json(url, data_ary, keys) {
+      $.getJSON(
+        url,
+        function(json) {
+          var t = (new Date()).getTime();
+          for(var i=0; i<data_ary.length; i++) {
+            if(data_ary[i].length > _TOTAL_POINTS) {
+              data_ary[i] = data_ary[i].slice(1);
+            }
+            data_ary[i].push([t, json[keys[i]]]);
+          }
         }
+      );
+    }
 
-        setInterval(function() {
-          $.ajax({
-              type: 'GET',
-              url:  '/json/mpstat_total',
-              dataType: 'json',
-              success: function(json) {
-                      var usr = json["user"];
-                      var sys = json["sys"];
-                      var idle = json["idle"];
-                      var param = [usr,sys,idle];
-                      var i;
-                      for(i=0;i<param_count;i++) {
-                          line[i].append(new Date().getTime(), param[i]);
-                      }
-              },
-          });
-        }, 1000);
+    function get_data() {
+      get_json("json/mpstat_total", [usr, sys, idle], ["user", "sys", "idle"]);
+      get_json("json/diskstat_total", [d_rBs, d_wBs], ["rbytes_s", "wbytes_s"]);
+      get_json("json/netstat_total", [n_rBs, n_wBs], ["rbytes_s", "wbytes_s"]);
+    }
 
-        var line_color = ["rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,0,255)", "rgb(0,255,255)"];
-        var fill_color = ["rgba(0,255,0,0.4)", "rgba(255,255,0,0.4)", 
-                          "rgba(255,0,255,0.4)", "rgba(0,255,255,0.4)"];
-        for(i=0;i<param_count;i++) {
-            smoothie.addTimeSeries(line[i], { strokeStyle: line_color[i], 
-                                fillStyle: fill_color[i], lineWidth: 3 });
+    // Set up the control widget
+    var updateInterval = 1000;
+    $("#updateInterval").val(updateInterval).change(function () {
+      var v = $(this).val();
+      if (v && !isNaN(+v)) {
+        updateInterval = +v;
+        if (updateInterval < 1) {
+          updateInterval = 1;
+        } else if (updateInterval > 2000) {
+          updateInterval = 2000;
         }
-        canvas_name = "mpstat_total";
-        smoothie.streamTo(document.getElementById(canvas_name), 0);
-    })();
+        $(this).val("" + updateInterval);
+      }
+    });
 
-    //(function(){
-    //    // smoothie.js + loadavg
-    //    var smoothie = new SmoothieChart(
-    //                       {timestampFormatter:SmoothieChart.timeFormatter,
-    //                        //maxValue: 100, 
-    //                        minValue: 0, 
-    //                        labels: {fontSize: 14},
-    //                        grid: { sharpLines:true,strokeStyle: 'rgb(0, 125, 0)',
-    //                        lineWidth: 1, //fillStyle: 'rgb(60, 0, 0)', 
-    //                        millisPerLine: 1000, verticalSections: 5 },
-    //                    });
+    // options
+    function gen_base_options() {
+      var base_option = {
+        legend: { show: true, position: "nw"},
+        series: { shadowSize: 0 },
+        yaxis: {
+          min: 0,
+          axisLabelUseCanvas: true,
+          axisLabelFontSizePixels: 12,
+          axisLabelFontFamily: 'Verdana, Arial',
+          axisLabelPadding: 6,
+        },
+        xaxis: { 
+          mode: "time",
+          tickSize: [2, "second"],
+          tickFormatter: function (v, axis) {
+            var date = new Date(v);
 
-    //    var param_count = 3;
-    //    var line = [];
-    //    var i;
-    //    for(i=0;i<param_count;i++) {
-    //        line.push(new TimeSeries());
-    //    }
+            if (date.getSeconds() % 20 == 0) {
+              var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+              var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+              var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-    //    setInterval(function() {
-    //      $.ajax({
-    //          type: 'GET',
-    //          url:  '/json/loadavg',
-    //          dataType: 'json',
-    //          success: function(json) {
-    //                  var _1min,_5min_15min;
-    //                  var param = [json["1min"],json["5min"], json["15min"]]
-    //                  for(i=0;i<param_count;i++) {
-    //                      line[i].append(new Date().getTime(), param[i]);
-    //                  }
-    //          },
-    //      });
-    //    }, 1000);
-
-    //    var line_color = ["rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,0,255)", "rgb(0,255,255)"];
-    //    var fill_color = ["rgba(0,255,0,0.4)", "rgba(255,255,0,0.4)", 
-    //                      "rgba(255,0,255,0.4)", "rgba(0,255,255,0.4)"];
-    //    for(i=0;i<param_count;i++) {
-    //        smoothie.addTimeSeries(line[i], { strokeStyle: line_color[i], 
-    //                            fillStyle: fill_color[i], lineWidth: 3 });
-    //    }
-    //    canvas_name = "loadavg";
-    //    smoothie.streamTo(document.getElementById(canvas_name), 0);
-    //})();
-
-    (function(){
-        // smoothie.js + diskstat
-        var smoothie = new SmoothieChart(
-                           {timestampFormatter:SmoothieChart.timeFormatter,
-                            //maxValue: 100, 
-                            minValue: 0, 
-                            labels: {fontSize: 14},
-                            grid: { sharpLines:true,strokeStyle: 'rgb(0, 125, 0)',
-                            lineWidth: 1, //fillStyle: 'rgb(60, 0, 0)', 
-                            millisPerLine: 1000, verticalSections: 5 },
-                        });
-        var i;
-        var param_count = 2;
-        var line = [];
-        for(i=0;i<param_count;i++) {
-            line.push(new TimeSeries());
+              return hours + ":" + minutes + ":" + seconds;
+            } else {
+              return "";
+            }
+          },
+          axisLabel: "Time",
+          axisLabelUseCanvas: true,
+          axisLabelFontSizePixels: 12,
+          axisLabelFontFamily: 'Verdana, Arial',
+          axisLabelPadding: 10
         }
+      };
+      return base_option;
+    }
 
-        setInterval(function() {
-          $.ajax({
-              type: 'GET',
-              url:  '/json/diskstat_total',
-              dataType: 'json',
-              success: function(json) {
-                      var rBs = json["rbytes_s"];
-                      var wBs = json["wbytes_s"];
-                      var param = [rBs, wBs];
-                      var i;
-                      for(i=0;i<param_count;i++) {
-                          line[i].append(new Date().getTime(), param[i]);
-                      }
-              },
-          });
-        }, 1000);
-
-        var line_color = ["rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,0,255)", "rgb(0,255,255)"];
-        var fill_color = ["rgba(0,255,0,0.4)", "rgba(255,255,0,0.4)", 
-                          "rgba(255,0,255,0.4)", "rgba(0,255,255,0.4)"];
-        for(i=0;i<param_count;i++) {
-            smoothie.addTimeSeries(line[i], { strokeStyle: line_color[i], 
-                                fillStyle: fill_color[i], lineWidth: 3 });
+    var options = [gen_base_options(), gen_base_options(), gen_base_options()];
+    options[0]["yaxis"]["axisLabel"] = "CPU usage";
+    options[0]["yaxis"]["max"] = 100;
+    options[0]["yaxis"]["min"] = 0;
+    options[0]["yaxis"]["tickFormatter"] = 
+      function (v, axis) {
+        if (v % 10 == 0) {
+          return v + "%";
+        } else {
+          return "";
         }
-        canvas_name = "diskstat_total";
-        smoothie.streamTo(document.getElementById(canvas_name), 0);
-    })();
+      };
+    options[0]["legend"]["container"] = leg_placeholders[0];
+    options[0]["legend"]["noColumns"] = 3;
+    options[1]["yaxis"]["axisLabel"] = "Disk I/O";
+    options[1]["legend"]["container"] = leg_placeholders[1];
+    options[1]["legend"]["noColumns"] = 2;
+    options[2]["yaxis"]["axisLabel"] = "Network I/O";
+    options[2]["legend"]["container"] = leg_placeholders[2];
+    options[2]["legend"]["noColumns"] = 2;
 
-    (function(){
-        // smoothie.js + netstat
-        var smoothie = new SmoothieChart(
-                           {timestampFormatter:SmoothieChart.timeFormatter,
-                            //maxValue: 100, 
-                            minValue: 0, 
-                            labels: {fontSize: 14},
-                            grid: { sharpLines:true,strokeStyle: 'rgb(0, 125, 0)',
-                            lineWidth: 1, //fillStyle: 'rgb(60, 0, 0)', 
-                            millisPerLine: 1000, verticalSections: 5 },
-                        });
-        var i;
-        var param_count = 2;
-        var line = [];
-        for(i=0;i<param_count;i++) {
-            line.push(new TimeSeries());
-        }
+    // Set up plot
+    var plots = [];
+    plots.push($.plot(placeholders[0], 
+          [{label:"usr",data:usr},{label:"sys",data:sys},{label:"idle", data:idle}], options[0]));
+    plots.push($.plot(placeholders[1], 
+          [{label:"rB/s",data:d_rBs},{label:"wB/s",data:d_wBs}], options[1]));
+    plots.push($.plot(placeholders[2], 
+          [{label:"recv(B/s)",data:n_rBs},{label:"send(B/s)",data:n_wBs}], options[2]));
 
-        setInterval(function() {
-          $.ajax({
-              type: 'GET',
-              url:  '/json/netstat_total',
-              dataType: 'json',
-              success: function(json) {
-                      var rBs = json["rbytes_s"];
-                      var wBs = json["wbytes_s"];
-                      var param = [rBs, wBs];
-                      var i;
-                      for(i=0;i<param_count;i++) {
-                          line[i].append(new Date().getTime(), param[i]);
-                      }
-              },
-          });
-        }, 1000);
+    function update() {
+      get_data();
+      plots[0].setData([{label:"usr",data:usr}, {label:"sys",data:sys}, {label:"idle",data:idle}]);
+      plots[1].setData([{label:"rB/s", data: d_rBs}, {label:"wB/s", data:d_wBs}]);
+      plots[2].setData([{label:"recv(B/s)", data:n_rBs}, {label:"recv(B/s)", data:n_wBs}]);
+      for(var i=0;i<plots.length; i++) {
+        plots[i].setupGrid();
+        plots[i].draw();
+      }
+      setTimeout(update, updateInterval);
+    }
+    update();
 
-        var line_color = ["rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,0,255)", "rgb(0,255,255)"];
-        var fill_color = ["rgba(0,255,0,0.4)", "rgba(255,255,0,0.4)", 
-                          "rgba(255,0,255,0.4)", "rgba(0,255,255,0.4)"];
-        for(i=0;i<param_count;i++) {
-            smoothie.addTimeSeries(line[i], { strokeStyle: line_color[i], 
-                                fillStyle: fill_color[i], lineWidth: 3 });
-        }
-        canvas_name = "netstat_total";
-        smoothie.streamTo(document.getElementById(canvas_name), 0);
-    })();
-
+  })();
 });
